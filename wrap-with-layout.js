3cf33b9ecb357d3c845da09c3c435fe5861106eb
xristos1994 <44114413+xristos1994@users.noreply.github.com>
2021-03-08 00:10:46 +0200

    update gatsby

--- wrap-with-layout.js
+++ wrap-with-layout.js
@@ -1,6 +1,8 @@
 const React = require("react");
 const Layout = require("./src/components").Layout;
 
-export default ({ element, props }) => {
+const wrapWithLayout = ({ element, props }) => {
   return <Layout {...props}>{element}</Layout>;
 };
+
+export default wrapWithLayout;
--- wrap-with-provider.js
+++ wrap-with-provider.js
@@ -4,9 +4,11 @@ import { Provider } from "react-redux";
 import { store } from "@store";
 
 // eslint-disable-next-line react/display-name,react/prop-types
-export default ({ element }) => {
+const wrapWithProvider = ({ element }) => {
   // Instantiating store in `wrapRootElement` handler ensures:
   //  - there is fresh store for each SSR page
   //  - it will be called only once in browser, when React mounts
   return <Provider store={store}>{element}</Provider>;
 };
+
+export default wrapWithProvider;
