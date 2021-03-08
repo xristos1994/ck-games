a7d3788d020c57a0ee8d29cd6c208d5e75553c49
xristos1994 <44114413+xristos1994@users.noreply.github.com>
2021-03-09 01:30:05 +0200

    prevent audio elements in ssr

--- src/models/tik-tak-boom/utils/audios.js
+++ src/models/tik-tak-boom/utils/audios.js
@@ -2,11 +2,19 @@ import UIfx from "uifx";
 import tikTakAudio from "@sounds/tik-tak.mp3";
 import boomAudio from "@sounds/boom.mp3";
 
-export const audios = {
-  tikTak: new UIfx(tikTakAudio, {
-    volume: 1,
-  }),
-  boom: new UIfx(boomAudio, {
-    volume: 1,
-  }),
+const ssrFakeAudio = {
+  tikTak: { play: () => {} },
+  boom: { play: () => {} },
 };
+
+export const audios =
+  typeof window !== "undefined"
+    ? {
+        tikTak: new UIfx(tikTakAudio, {
+          volume: 1,
+        }),
+        boom: new UIfx(boomAudio, {
+          volume: 1,
+        }),
+      }
+    : { ...ssrFakeAudio };
