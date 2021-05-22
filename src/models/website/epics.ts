023a4604289820aaa72429cbebff2a7e234b3941
xristos1994 <xristos.korompokis@gmail.com>
2021-05-22 18:43:43 +0300

    Restart all games when navigate to homepage

--- src/models/website/epics.ts
+++ src/models/website/epics.ts
@@ -5,23 +5,34 @@ import {
   StateObservable,
   ofType,
 } from "redux-observable";
-import { map } from "rxjs/operators";
+import { mergeMap } from "rxjs/operators";
 import { IActionWithPayload } from "@core/actions/interfaces";
-import { startWebsite, updateSelectedGame } from "./actions";
+import { startWebsite, updateSelectedGame, restartAllGames } from "./actions";
 import { IState, AvailableGames } from "./interfaces";
-import { initializeGame as initializePantomime } from "@models/pantomime/actions";
-import { initializeGame as initializeTikTakBoom } from "@models/tik-tak-boom/actions";
+import {
+  initializeGame as initializePantomime,
+  restartGame as restartPantomime,
+} from "@models/pantomime/actions";
+import {
+  initializeGame as initializeTikTakBoom,
+  restartGame as restartTikTakBoom,
+} from "@models/tik-tak-boom/actions";
 
 const startEpic = (): Observable<IActionWithPayload> => of(startWebsite(null));
 
 const initializePantomimeEpic = (
   action$: ActionsObservable<IActionWithPayload>,
   state$: StateObservable<IState>
-): Observable<IActionWithPayload<IState["selectedGame"]>> => {
+): Observable<
+  IActionWithPayload | IActionWithPayload<IState["selectedGame"]>
+> => {
   return action$.pipe(
     ofType(initializePantomime.type),
-    map(() => {
-      return updateSelectedGame(AvailableGames.pantomime);
+    mergeMap(() => {
+      return [
+        restartTikTakBoom(null),
+        updateSelectedGame(AvailableGames.pantomime),
+      ];
     })
   );
 };
@@ -29,11 +40,28 @@ const initializePantomimeEpic = (
 const initializeTikTakBoomEpic = (
   action$: ActionsObservable<IActionWithPayload>,
   state$: StateObservable<IState>
-): Observable<IActionWithPayload<IState["selectedGame"]>> => {
+): Observable<
+  IActionWithPayload | IActionWithPayload<IState["selectedGame"]>
+> => {
   return action$.pipe(
     ofType(initializeTikTakBoom.type),
-    map(() => {
-      return updateSelectedGame(AvailableGames.tikTakBoom);
+    mergeMap(() => {
+      return [
+        restartPantomime(null),
+        updateSelectedGame(AvailableGames.tikTakBoom),
+      ];
+    })
+  );
+};
+
+const restartAllGamesEpic = (
+  action$: ActionsObservable<IActionWithPayload>,
+  state$: StateObservable<IState>
+): Observable<IActionWithPayload> => {
+  return action$.pipe(
+    ofType(restartAllGames.type),
+    mergeMap(() => {
+      return [restartPantomime(null), restartTikTakBoom(null)];
     })
   );
 };
@@ -41,5 +69,6 @@ const initializeTikTakBoomEpic = (
 export const websiteEpic = combineEpics(
   startEpic,
   initializePantomimeEpic,
-  initializeTikTakBoomEpic
+  initializeTikTakBoomEpic,
+  restartAllGamesEpic
 );
