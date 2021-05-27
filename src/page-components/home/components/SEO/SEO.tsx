24fd2adc0316a275ab8c210329a19fd76cbe78f8
xristos1994 <xristos.korompokis@gmail.com>
2021-05-27 20:01:58 +0300

    Fix typo

--- src/page-components/home/components/SEO/SEO.tsx
+++ src/page-components/home/components/SEO/SEO.tsx
@@ -5,7 +5,7 @@ import { getCanonicalBaseUrl } from "@utils/general";
 const title = "CK-Games";
 const canonicalUrl = `${getCanonicalBaseUrl()}/`;
 const description =
-  "Εσύ επιλέγεις πως θα διασκεδάσεις με την παρέα σου. Παίξτε Παντομίμα ή Tik-Tak-Boom?";
+  "Εσύ επιλέγεις πως θα διασκεδάσεις με την παρέα σου. Παίξτε Παντομίμα ή Tik-Tak-Boom.";
 const keywords =
   "παιχνίδι, διασκέδαση, φίλοι, παρέα, παντομίμα, μίμηση, ταινία, νίκη, ήττα, ισοπαλία, tik-tak-boom, tik, tak, boom, βόμβα";
 const ogImage = `${getCanonicalBaseUrl()}${
