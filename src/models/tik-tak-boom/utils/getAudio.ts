20ea54c3fdd9bacc02547b39a9e6f8b7d499fba3
xristos1994 <xristos.korompokis@gmail.com>
2021-06-16 19:12:28 +0300

    remove audio elements from dom

--- src/models/tik-tak-boom/utils/getAudio.ts
+++ src/models/tik-tak-boom/utils/getAudio.ts
@@ -4,6 +4,11 @@ const tikTakAudio = require("@sounds/tik-tak.mp3").default;
 const boomAudio = require("@sounds/boom.mp3").default;
 
 export const getAudio = (audioKey: "tikTak" | "boom"): UIfx | undefined => {
+  //TODO  add remove polyfill if needed
+  Array.prototype.forEach.call(document.querySelectorAll("audio"), audioElem =>
+    audioElem.remove()
+  );
+
   return {
     tikTak: new UIfx(tikTakAudio, {
       volume: 1,
