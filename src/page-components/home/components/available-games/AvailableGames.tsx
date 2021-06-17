d5d73104dee31fe172af718098df2241ec67fad2
xristos1994 <xristos.korompokis@gmail.com>
2021-06-17 15:57:30 +0300

    Add translation in Available games component

--- src/page-components/home/components/available-games/AvailableGames.tsx
+++ src/page-components/home/components/available-games/AvailableGames.tsx
@@ -1,18 +1,25 @@
 import React, { FC, ReactElement } from "react";
 import { Link } from "gatsby";
-import { classnames } from "@utils/component-utils";
+import { compose } from "@utils/component-utils";
+import { withTranslation, ITranslate } from "@models/i18n/hoc";
 import { Button } from "@components";
 import { availableGames } from "./config";
 const styles = require("./styles.module.css");
 
-const AvailableGames: FC = (): ReactElement => {
+interface IProps {
+  t: ITranslate;
+}
+
+const _AvailableGames: FC<IProps> = ({ t }): ReactElement => {
   const games = availableGames.map(({ url, name }) => (
     <Link to={url} key={name}>
-      <Button className={classnames(styles.gameButton)}>{name}</Button>
+      <Button className={styles.gameButton}>{t(name)}</Button>
     </Link>
   ));
 
   return <div className={styles.availableGamesContainer}>{games}</div>;
 };
+
+const AvailableGames = compose(withTranslation)(_AvailableGames);
 
 export { AvailableGames };
