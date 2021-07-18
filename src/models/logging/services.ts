78891de87607e780ed92fd40d5c786b88f7af80d
xristos1994 <xristos.korompokis@gmail.com>
2021-07-18 03:17:23 +0300

    remove console.log

--- src/models/logging/services.ts
+++ src/models/logging/services.ts
@@ -3,7 +3,6 @@ import { post } from '@core/services';
 const LOGGING_BASE_URL = 'https://apis.stackprint.io/ck-logging';
 
 export const createLog = (body: Record<string, unknown>): unknown => {
-  console.log(body);
   return post({
     url: `${LOGGING_BASE_URL}/logs`,
     headers: { 'Content-Type': 'application/json' },
