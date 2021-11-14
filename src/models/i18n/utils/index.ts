6a0c6b3879f75adb40de1c695e447b9134188f38
xristos1994 <xristos.korompokis@gmail.com>
2021-11-14 12:45:42 +0200

    Test rename file

--- src/models/i18n/utils/index.ts
+++ src/models/i18n/utils/index.ts
@@ -1,6 +1,6 @@
 import { availableLangs } from './availableLangs';
 import { setLocalStorageLang, getLocalStorageLang } from './localStorageLang';
 import { LangFlagMap } from './LangFlagMap';
-import { getLangFromPathname } from './getLangFromPathname';
+import { getLangFromPathname } from './getLangFromPathname_';
 
 export { availableLangs, setLocalStorageLang, getLocalStorageLang, LangFlagMap, getLangFromPathname };
